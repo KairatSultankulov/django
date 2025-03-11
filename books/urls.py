1cae52e9fd1ff9f07e6aa6e52f51f8da41ffebb5
KairatSultankulov <sultankulov.kairat@gmail.com>
2025-03-11 15:36:35 +0600

    homework 2

--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,7 @@ from  . import views
 urlpatterns = [
     path('aboutme', views.about_me),
     path('mypet', views.my_pet),
-    path('time', views.time)
+    path('time', views.time),
+    path("book_list/", views.books_list),
+    path("book_list/<int:id>/", views.books_detail),
 ]
