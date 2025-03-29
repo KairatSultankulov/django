7ab25d9237a81931e0647c6308dfc95f11f243af
KairatSultankulov <sultankulov.kairat@gmail.com>
2025-03-29 20:51:10 +0600

    Оптимизация, debugging, кэширование

--- books/urls.py
+++ books/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('aboutme', views.about_me),
-    path('mypet', views.my_pet),
-    path('time', views.time),
-    path("", views.books_list),
-    path("book_list/<int:id>/", views.books_detail),
+    path('aboutme', views.AboutMeView.as_view(), name='aboutme'),
+    path('mypet', views.MyPetView.as_view(), name='mypet'),
+    path('time', views.TimeView.as_view(), name='time'),
+    path("", views.BookListView.as_view(), name='book_list'),
+    path("book_list/<int:id>/", views.BookDetailView.as_view(), name='book_detail'),
     path("search/", views.SearchBookView.as_view(), name="search"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,58 +1,66 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views import generic
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from datetime import datetime
 from . import models
-from django.views import generic
 
 class SearchBookView(generic.ListView):
     template_name = 'book.html'
     context_object_name = 'query'
 
     def get_queryset(self):
-        return models.Books.objects.filter(title__icontains=self.request.GET.get('q'))
+        query = self.request.GET.get('q')
+        cache_key = f'query_{query}'
+        books = cache.get(cache_key)
+        if not books:
+            books = models.Books.objects.filter(title__icontains=query)
+            cache.set(cache_key, books, 60 * 15)
+        return books
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['q'] = self.request.GET.get('q')
-        return context
+@method_decorator(cache_page(60 * 15), name='dispatch')
+class BookDetailView(generic.DetailView):
+    model = models.Books
+    template_name = 'book_detail.html'
+    context_object_name = 'book'
 
+@method_decorator(cache_page(60 * 15), name='dispatch')
+class BookListView(generic.ListView):
+    model = models.Books
+    template_name = 'book.html'
+    context_object_name = 'query'
 
-def books_detail(request, id):
-    if request.method == 'GET':
-        book_id = get_object_or_404(models.Books, id=id)
-        return render(
-            request,
-            template_name='book_detail.html',
-            context={
-                'book_id': book_id,
-            }
-        )
+    def get_queryset(self):
+        books = cache.get('query')
+        if not books:
+            books = self.model.objects.all()
+            cache.set('query', books, 60 * 15)
+        return books
 
-def books_list(request):
-    if request.method == "GET":
-        query = models.Books.objects.all()
-        return render (
-            request,
-            template_name="book.html",
-            context = {
-                "query": query,
-            }
-        )
+@method_decorator(cache_page(60 * 15), name='dispatch')
+class AboutMeView(generic.TemplateView):
+    template_name = 'about_me.html'
 
-
-def about_me(request):
-    if request.method == "GET":
+    def get(self, request, *args, **kwargs):
         return HttpResponse("Hi! My name is Kairat, I'm 23 years old. I'm always open to new opportunities and strive to grow in various areas of life. I love learning new things, finding creative solutions, and sharing experiences with others.")
 
-def my_pet(request):
-    if request.method == "GET":
+@method_decorator(cache_page(60 * 15), name='dispatch')
+class MyPetView(generic.TemplateView):
+    template_name = 'my_pet.html'
+
+    def get(self, request, *args, **kwargs):
         pet_name = 'REX'
         return HttpResponse(
             f"My pet name is {pet_name}"
             "<img src='https://www.bethowen.ru/upload/iblock/898/8982eee1d576c30e28811046c021a8af.jpg' />"
         )
 
-def time(request):
-    if request.method == "GET":
+@method_decorator(cache_page(60 * 15), name='dispatch')
+class TimeView(generic.TemplateView):
+    template_name = 'time.html'
+
+    def get(self, request, *args, **kwargs):
         current_time = datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
         return HttpResponse(current_time)
